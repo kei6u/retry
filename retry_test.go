f1af74bb2c8b0d9056076722d4f0bd4f5e217909
kei6u <keisuke.umegaki.630@gmail.com>
2022-02-03 09:04:30 +0900

    fix unit tests

--- retry_test.go
+++ retry_test.go
@@ -6,52 +6,43 @@ import (
 	"time"
 )
 
-func TestDefaultConstantRetry(t *testing.T) {
-	t.Parallel()
-	defaultContextWithTimeout = func() (context.Context, context.CancelFunc) {
-		return context.WithTimeout(context.TODO(), 10*time.Millisecond)
-	}
-	start := time.Now()
-	for DefaultConstant.Next() {
-	}
-	if time.Since(start) < 10*time.Millisecond {
-		t.Fatalf("expected to timeout after 100ms")
-	}
-}
-
-func TestConstantRetryWithMaxAttempts(t *testing.T) {
-	t.Parallel()
-	retryCount := 0
-	constant := Constant(ConstantOptions{
-		Interval:    time.Millisecond,
-		MaxAttempts: 10,
+func TestConstant(t *testing.T) {
+	t.Run("use DefaultConstant of global variable", func(t *testing.T) {
+		overwrite_defaltContextWithTimeout(t, 10*time.Millisecond)
+		start := time.Now()
+		for DefaultConstant.Next() {
+		}
+		if time.Since(start) < 10*time.Millisecond {
+			t.Fatalf("expected to timeout after 100ms")
+		}
 	})
-	for constant.Next() {
-		retryCount++
-	}
-	if retryCount != 10 {
-		t.Fatalf("expected 10 retries, but %d", retryCount)
-	}
-}
-
-func TestConstantRetryWithTimeoutContext(t *testing.T) {
-	t.Parallel()
-	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Millisecond)
-	defer cancel()
-	constant := Constant(ConstantOptions{
-		Context:  ctx,
-		Interval: time.Second,
+	t.Run("set max attempts only", func(t *testing.T) {
+		retryCount := 0
+		constant := Constant(ConstantOptions{
+			Interval:    time.Millisecond,
+			MaxAttempts: 10,
+		})
+		for constant.Next() {
+			retryCount++
+		}
+		if retryCount != 10 {
+			t.Fatalf("expected 10 retries, but %d", retryCount)
+		}
 	})
-	start := time.Now()
-	for constant.Next() {
-	}
-	if time.Since(start) < 10*time.Millisecond {
-		t.Fatal("expected timeout after 10ms")
-	}
-}
-
-func TestConstantRetryMaxAttemptsAndWithTimeoutContext(t *testing.T) {
-	t.Parallel()
+	t.Run("set timeout only", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Millisecond)
+		defer cancel()
+		constant := Constant(ConstantOptions{
+			Context:  ctx,
+			Interval: time.Second,
+		})
+		start := time.Now()
+		for constant.Next() {
+		}
+		if time.Since(start) < 10*time.Millisecond {
+			t.Fatal("expected timeout after 10ms")
+		}
+	})
 	t.Run("prioritize max attempts", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.TODO(), time.Second)
 		defer cancel()
@@ -80,6 +71,7 @@ func TestConstantRetryMaxAttemptsAndWithTimeoutContext(t *testing.T) {
 		start := time.Now()
 		retryCount := 0
 		for constant.Next() {
+			t.Logf("retry %d, %s elapsed", retryCount, time.Since(start))
 			retryCount++
 		}
 		if 10 <= retryCount && 10*time.Millisecond < time.Since(start) {
@@ -89,54 +81,50 @@ func TestConstantRetryMaxAttemptsAndWithTimeoutContext(t *testing.T) {
 	})
 }
 
-func TestExponentialBackoffRetry(t *testing.T) {
-	t.Parallel()
-	defaultContextWithTimeout = func() (context.Context, context.CancelFunc) {
-		return context.WithTimeout(context.TODO(), 10*time.Millisecond)
-	}
-	start := time.Now()
-	for DefaultExponentialBackoff.Next() {
-	}
-	if time.Since(start) < 10*time.Millisecond {
-		t.Fatalf("expected to timeout after 10ms")
-	}
-}
-
-func TestExponentialBackoffRetryWithMaxAttempts(t *testing.T) {
-	t.Parallel()
-	backoff := ExponentialBackoff(ExponentialBackoffOptions{
-		BaseInterval: time.Millisecond,
-		MaxAttempts:  10,
+func TestExponentialBackoff(t *testing.T) {
+	t.Run("use DefaultExponentialBackoff of global variable", func(t *testing.T) {
+		overwrite_defaltContextWithTimeout(t, 10*time.Millisecond)
+		start := time.Now()
+		for DefaultExponentialBackoff.Next() {
+		}
+		if time.Since(start) < 10*time.Millisecond {
+			t.Fatalf("expected to timeout after 10ms")
+		}
 	})
-	start := time.Now()
-	retryCount := 0
-	for backoff.Next() {
-		retryCount++
-	}
-	if retryCount != 10 && time.Since(start) < 600*time.Millisecond {
-		t.Fatal("expected 10 retries and elapse at least 600ms")
-	}
-}
-
-func TestExponentialBackoffRetryWithTimeout(t *testing.T) {
-	t.Parallel()
-	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Millisecond)
-	defer cancel()
-	backoff := ExponentialBackoff(ExponentialBackoffOptions{
-		Context:      ctx,
-		BaseInterval: time.Second,
+	t.Run("set max attempts only", func(t *testing.T) {
+		backoff := ExponentialBackoff(ExponentialBackoffOptions{
+			BaseInterval: time.Millisecond,
+			MaxAttempts:  10,
+		})
+		start := time.Now()
+		retryCount := 0
+		for backoff.Next() {
+			t.Logf("retry %d, %s elapsed", retryCount, time.Since(start))
+			retryCount++
+		}
+		if retryCount != 10 && time.Since(start) < 600*time.Millisecond {
+			t.Fatalf(
+				"expected 10 retries, elapse at least 600ms, actual: %d retries, %s elapsed",
+				retryCount,
+				time.Since(start),
+			)
+		}
 	})
-	start := time.Now()
-	for backoff.Next() {
-	}
-	if time.Since(start) < 10*time.Millisecond {
-		t.Fatal("expected elapse at least 10ms")
-	}
-}
-
-func TestExponentialBackoffRetryMaxAttemptsAndWithTimeoutContext(t *testing.T) {
-	t.Parallel()
-	t.Run("prioritize max attempts", func(t *testing.T) {
+	t.Run("set timeout only", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Millisecond)
+		defer cancel()
+		backoff := ExponentialBackoff(ExponentialBackoffOptions{
+			Context:      ctx,
+			BaseInterval: time.Second,
+		})
+		start := time.Now()
+		for backoff.Next() {
+		}
+		if time.Since(start) < 10*time.Millisecond {
+			t.Fatalf("expected to timeout 10ms, but %s elapsed", time.Since(start))
+		}
+	})
+	t.Run("max attempts is prioritized", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.TODO(), time.Second)
 		defer cancel()
 		backoff := ExponentialBackoff(ExponentialBackoffOptions{
@@ -153,7 +141,7 @@ func TestExponentialBackoffRetryMaxAttemptsAndWithTimeoutContext(t *testing.T) {
 			t.Fatal("expected to reach 10 retries before 1 second elapsed")
 		}
 	})
-	t.Run("prioritize timeout", func(t *testing.T) {
+	t.Run("timeout is prioritized", func(t *testing.T) {
 		ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Millisecond)
 		defer cancel()
 		backoff := ExponentialBackoff(ExponentialBackoffOptions{
@@ -168,6 +156,18 @@ func TestExponentialBackoffRetryMaxAttemptsAndWithTimeoutContext(t *testing.T) {
 		}
 		if 10 <= retryCount && 10*time.Millisecond < time.Since(start) {
 			t.Fatal("expected to timeout before 10 retries")
+		}
+	})
+}
+
+func overwrite_defaltContextWithTimeout(t *testing.T, d time.Duration) {
+	defaultContextWithTimeout = func() (context.Context, context.CancelFunc) {
+		return context.WithTimeout(context.TODO(), d)
+	}
+	t.Cleanup(func() {
+		// reset to default not to effect other tests.
+		defaultContextWithTimeout = func() (context.Context, context.CancelFunc) {
+			return context.WithTimeout(context.TODO(), time.Minute)
 		}
 	})
 }
